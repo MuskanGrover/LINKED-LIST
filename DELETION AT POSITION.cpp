3471a30e2abbe0b33e0251f4c300ecb711ae3257
Muskan Grover <149904645+MuskanGrover@users.noreply.github.com>
2023-11-27 01:58:39 +0530

    Add files via upload

---
+++ DELETION AT POSITION.cpp
@@ -0,0 +1,60 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+	int data;
+	node*next;
+};
+int main()
+{
+	int n,val;
+	node*p,*q,*head;
+	printf("\nENTER THE TOTAL NUMBER OF NODES");
+	scanf("%d",&n);
+	printf("\nENTER THE DATA PART OF NODE ");
+	scanf("%d",&val);
+	
+	p=(struct node*)malloc(sizeof(struct node));
+	p->data=val;
+	p->next=NULL;
+	head=p;
+	for(int i=1;i<n;i++)
+	{
+		q=(struct node*)malloc(sizeof(struct node));
+		printf("\nENTER THE DATA PART OF NODE ");
+	scanf("%d",&val);
+	q->data=val;
+	q->next=NULL;
+	p->next=q;
+	p=p->next;
+	}
+	p=head;
+	while(p!=NULL)
+	{
+		printf("\n%d",p->data);
+		p=p->next;
+	}
+	//DELETION OF A NODE AT POSITION
+	int pos;
+	node*temp=head;
+	node*prev=head;
+	printf("\nENTER THE POSITION OF NODE  ");
+	scanf("%d",&pos);
+	for(int i=0;i<pos;i++)
+	{
+		prev=temp;
+		temp=temp->next;
+	}
+	prev->next=temp->next;
+	temp->next=prev;
+	free(temp);
+	
+	//LINKED LIST AFTER DELETION OF NODE FROM BEGINNING
+	p=head;
+	while(p!=NULL)
+	{
+		printf("\n %d",p->data);
+		p=p->next;
+	}
+}
+	
